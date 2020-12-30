4e32b3ef8b00531b5a7d998951dbc5267e93eae6
rockeven199 <2092056202@qq.com>
2020-12-30 16:08:53 +0800

    界面改版

---
+++ Elecsite Test-Build update/update/js/register.js
@@ -0,0 +1,11 @@
+function submit() {
+    var confrim1 = document.getElementById('input_usrname');
+    var confrim2 = document.getElementById('input_password');
+    if (confrim1 == confrim2) {
+
+    }
+    else {
+        document.getElementById('input_usrname');
+        document.getElementById('')
+    }
+}
